54d6b3f8f52cccb663bba832e0378e6c6ab464d9
A1ien0101 <122251355+A1ien0101@users.noreply.github.com>
2026-03-30 13:06:56 +0800

    tcp_scanner_Original.go

---
+++ tcp-scanner/tcp_scanner_Original.go
@@ -0,0 +1,58 @@
+/*
+Black Hat Go 的初始版本
+
+第一次运行后, 出现了长时间无反应问题
+但是, 之后几天运行, 却没有问题
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"sort"
+)
+
+func worker(ports, results chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
+	}
+}
+
+func main() {
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
+	}
+}
